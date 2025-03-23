c5950a77d7d0b8aaaf1610feba2c15d26c78f969
Samsak-S <samsak198@gmail.com>
2025-03-23 19:35:15 +0530

    some tweaks

--- index.js
+++ index.js
@@ -6,10 +6,21 @@ const list_view = list.childNodes;
 list_view.forEach(child => {
     if(child.nodeName === "#text")
         child.remove();
-    console.log(child)
 });
 
-console.log(list_view);
+function addElement(text_input) {
+    const new_div = document.createElement("div");
+    new_div.id = `item-${++id}`;
+    new_div.className = "item";
+
+    new_div.innerHTML = `<p>${text_input}</p>
+    <button class="delete">x</button>`;
+
+    const parent_div = document.querySelector("#list-view");
+    parent_div.appendChild(new_div);
+    task.value = "";
+    console.log(new_div);
+}
 
 const click = document.querySelector("#add-item");
 
@@ -19,33 +30,24 @@ click.addEventListener("click", (event) => {
     const text_input = task.value.trim();
 
     if(text_input) {
-        const new_div = document.createElement("div");
-        new_div.id = `item-${++id}`;
-        new_div.className = "item";
-
-        new_div.innerHTML = `<p>${text_input}</p>
-        <button class="delete">x</button>`;
-
-        const parent_div = document.querySelector("#list-view");
-        parent_div.appendChild(new_div);
-        task.value = "";
+        addElement(text_input);
     }
-
-    const clear_all = document.querySelector("#clear-all");
-    clear_all.addEventListener("click", (event) => {
-        document.querySelectorAll(".delete").forEach(ele => {
-            ele.parentElement.remove();
-        });
-    });
     document.querySelectorAll(".delete").forEach(ele => {
         ele.addEventListener("click", (event) => {
             event.target.parentElement.remove();
         });
     });
+
+});
+
+const clear_all = document.querySelector("#clear-all");
+clear_all.addEventListener("click", (event) => {
+    document.querySelectorAll(".delete").forEach(ele => {
+        ele.parentElement.remove();
+    });
 });
 
 const filter = document.querySelector("#filter");
-
 filter.addEventListener("input", (event) => {
     event.preventDefault();
     const text = document.querySelector("#filter").value.trim();
